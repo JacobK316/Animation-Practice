a7e5c852390f9c46a58280e9b47673638905d165
Jacob Kappler <jacobkappler@gmail.com>
2026-02-17 21:45:18 -0500

    Initial Commit

---
+++ Animation Practice/Animation_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_PracticeApp.swift
+//  Animation Practice
+//
+//  Created by Jacob Kappler on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Animation_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
