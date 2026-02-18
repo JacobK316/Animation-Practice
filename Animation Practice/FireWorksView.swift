ae8d1f9f69d2c181645449a2b8695a75336accae
Jacob Kappler <jacobkappler@gmail.com>
2026-02-18 16:43:34 -0500

    added new animations

---
+++ Animation Practice/FireWorksView.swift
@@ -0,0 +1,18 @@
+//
+//  FireWorksView.swift
+//  Animation Practice
+//
+//  Created by Jacob Kappler on 2/18/26.
+//
+
+import SwiftUI
+
+struct FireWorksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FireWorksView()
+}
---
+++ Animation Practice/MatchedView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchedView.swift
+//  Animation Practice
+//
+//  Created by Jacob Kappler on 2/18/26.
+//
+
+import SwiftUI
+
+struct MatchedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchedView()
+}
